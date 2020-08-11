c2c4656a021f7f87b11e6a2ccfadfa7eee0ddfb8
Wirlley Delfino <wirlley_d@hotmail.com>
2020-08-11 10:59:43 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({ webPreferences: { nodeIntegration: true } })
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+})
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, function (err, metadata) {
+    mainWindow.webContents.send("video:duration", metadata.format.duration);
+  });
+});
